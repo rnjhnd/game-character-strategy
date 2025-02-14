ba473deb620ec43af2131692a4f421d7b43eb10d
Aren John Esoen <152838648+ArenJohnD@users.noreply.github.com>
2025-02-14 12:57:33 +0800

    Update KnightDefense.java

--- KnightDefense.java
+++ KnightDefense.java
@@ -1,5 +1,5 @@
 public class KnightDefense implements DefenseStrategy {
-    public void defend(){
+    public void defend() {
         System.out.println("Using a shield to defend!");
         System.out.println("Dodging to avoid attack!");
         System.out.println("Creating a magic barrier for defense!");
